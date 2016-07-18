3bb8c47bad9680daaf6a981225ee6d8798636d3e
Mandych00ng <choong.mandy.yee@gmail.com>
2016-07-18 17:48:49 +0800

    installed Figaro gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static/home'
+
+  get 'static/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#home'
+  
 end
